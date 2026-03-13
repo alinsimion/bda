ce9b823f9497c9800cddef6f52ad90851d82188d
Ciprian Alin Simion <alinsimionciprian@gmail.com>
2026-03-13 08:40:10 +0200

    Updates

---
+++ mongo/assets/md-Ba02KTb3.js
@@ -0,0 +1,10 @@
+import{b as i,o as r,w as o,g as l,i as s,v as u,x as d,C as e}from"./modules/vue-DRbesB1H.js";import{I as c}from"./slidev/default-e7bD-WGV.js";import{u as x,f as m}from"./slidev/context-B6vGDs6A.js";import"./index-V9kIJ8Wy.js";import"./modules/shiki-CTrNIJo1.js";const B={__name:"slides.md__slidev_6",setup(p){const{$clicksContext:n,$frontmatter:a}=x();return n.setup(),(b,t)=>(r(),i(c,u(d(e(m)(e(a),5))),{default:o(()=>[t[0]||(t[0]=l("h1",null,"JSON vs BSON - 1",-1)),s(` | <div class="text-2xl font-extrabold"> JSON </div>| <div class="text-2xl font-extrabold"> BSON </div> |
+|---------------------|-------------------|
+| String              | MinKey             |
+| Number              | 12.MaxKey           |
+| Object (JSON Object)| Binary Data               |
+| Array               | ObjectId              |
+| Boolean             | Regular Expression               |
+| Null                | Javascript Code               |
+|                     | Decimal128               |
+|                     | Date               | `),t[1]||(t[1]=l("div",{class:"grid grid-cols-2 gap-4"},[l("div",null,[l("div",{class:"text-2xl font-extrabold"}," JSON "),l("br"),l("ul",null,[l("li",null,"String"),l("li",null,"Number"),l("li",null,"Object (JSON object)"),l("li",null,"Array"),l("li",null,"Boolean"),l("li",null,"Null")])]),l("div",null,[l("div",{class:"text-2xl font-extrabold"}," BSON "),l("br"),l("ul",null,[l("li",null,[l("span",null,"JSON FIELDS")]),l("li",null,"Minkey"),l("li",null,"Maxkey"),l("li",null,"Binary Data"),l("li",null,"ObjectID"),l("li",null,"Regular Expression"),l("li",null,"JavaScript code"),l("li",null,"Decimal128"),l("li",null,"Date")])])],-1))]),_:1},16))}};export{B as default};
